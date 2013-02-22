a73923c027b9e798ca4ec057aad214dc2b4cf1f5
Andrew Sullivan <licyeus@gmail.com>
2013-02-21 18:37:01 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ibikealot::Application.config.secret_token = '0d35e40ba59c90871afc297a9c4424705a5ffcc8b8ccba00b95f8f5bfe380827a4bc61e9784e3607bf62aaf4d376352c20025638bc67c2a91cfb5ead55deb85e'
